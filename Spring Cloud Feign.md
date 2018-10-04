ff232c1b8aa83ae995d2ecd2dade60aba3ff85c8
sh <18852953917@163.com>
2018-10-04 18:28:21 +0800

    Ribbon 配置

--- Spring Cloud Feign.md
+++ Spring Cloud Feign.md
@@ -15,3 +15,19 @@ Spring Cloud Feign整合了Spring Cloud Ribbon和Spring Cloud Hystrix的使用�
 # 参数绑定
 在进行参数绑定时，@RequestParam、@RequestHeader等可以指定参数名称的注解，但是它们的value必不可少。Spring MVC中这两个注解会根据参数名作为默认值，
 但是在Feign中绑定参数必须通过value属性来指明具体的参数名。
+
+# Ribbon配置
+Spring Cloud Feign的客户端负载均衡是通过Spring Cloud Ribbon实现，所以可以直接通过配置Ribbon客户端的方式来自定义各个客户端调用的参数。
+
+## 全局配置
+全局配置直接使用ribbon.<key>=<value>的方式来设置ribbon的各项默认参数
+
+## 指定服务配置
+指定服务配置使用<client>.ribbon.<key>=<value>的格式设置。
+
+<client>参数来源：在定义客户端的时候，使用了@FeignClient注解。在初始化过程中，Spring Cloud Feign会根据注解的name属性或者value属性指定的服务名，
+自动创建一个同名的Ribbon客户端。
+
+## 重试机制
+
+# Hystrix配置
--- hello-service/src/main/java/cn/sh/hello/service/HelloService.java
+++ hello-service/src/main/java/cn/sh/hello/service/HelloService.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 /**
  * @author sh
  */
@@ -13,6 +15,13 @@ public class HelloService {
     private static final Logger logger = LoggerFactory.getLogger(HelloService.class);
 
     public String hello() {
+        int sleep = new Random().nextInt(3000);
+        logger.info("sleep time:{}", sleep);
+        try {
+            Thread.sleep(sleep);
+        } catch (InterruptedException e) {
+            logger.error("线程休眠发生异常，error:{}", e);
+        }
         return "Hello, feign consumer";
     }
 
